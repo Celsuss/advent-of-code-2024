0ad5d5cd8e0f05c7f0438ef2c08e94466887daec
Celsuss <jenslorden@proton.me>
2024-12-20 17:54:03 +0100

    solved day two

--- day-two.py
+++ day-two.py
@@ -1,6 +1,12 @@
 """Advent of code 2024 day two."""
 
 
+def isIncreasing(report: list[int]) -> bool:
+    if report[0] > report[1]:
+        return False
+    return True
+
+
 def isReportSafe(report: list[int]) -> bool:
     """
     Is report safe.
@@ -9,12 +15,16 @@ def isReportSafe(report: list[int]) -> bool:
     - The levels are either all increasing or all decreasing.
     - Any two adjacent levels differ by at least one and at most three.
     """
-    safe = True
-    prev_i = report[0]
-    for i in report[1:]:
-
-        continue
-    return safe
+    incremental = isIncreasing(report)
+    prev_num = report[0]
+    for num in report[1:]:
+        delta = abs(num - prev_num)
+        if (incremental is True and num < prev_num) or \
+            (incremental is False and num > prev_num) or \
+            (delta > 3 or delta == 0):
+            return False
+        prev_num = num
+    return True
 
 
 def partOne(reports: list[list[int]]) -> int:
@@ -28,17 +38,60 @@ def partOne(reports: list[list[int]]) -> int:
     for report in reports:
         if isReportSafe(report):
             safe_count += 1
-        continue
-
     return safe_count
 
 
-def partOneTwo(reports: list[list[int]]) -> int:
-    """Part one solution."""
-    for report in reports:
-        continue
+def isReportSafeUpdated(report: list[int]) -> bool:
+    """
+    Is report safe.
 
-    return 0
+    A report only counts as safe if both of the following are true:
+    - The levels are either all increasing or all decreasing.
+    - Any two adjacent levels differ by at least one and at most three.
+
+    It is now possible to remove a single number from an unsafe report to try
+    and make it safe.
+    """
+    incremental = isIncreasing(report)
+    prev_num = report[0]
+    for i in range(1, len(report)):
+        num = report[i]
+        delta = abs(num - prev_num)
+
+        if (incremental is True and num < prev_num) or \
+            (incremental is False and num > prev_num) or \
+            (delta > 3 or delta == 0):
+            if i-2 >= 0:
+                new_report = report.copy()
+                del new_report[i-2]
+                if isReportSafe(new_report) is True:
+                    return True
+
+            new_report = report.copy()
+            del new_report[i-1]
+            if isReportSafe(new_report) is True:
+                return True
+
+            new_report = report.copy()
+            del new_report[i]
+            return isReportSafe(new_report)
+
+        prev_num = num
+    return True
+
+
+def partTwo(reports: list[list[int]]) -> int:
+    """Part one solution.
+
+    Now, the same rules apply as before, except if removing a single level from
+    an unsafe report would make it safe, the report instead counts as safe.
+
+    """
+    safe_count = 0
+    for report in reports:
+        if isReportSafeUpdated(report):
+            safe_count += 1
+    return safe_count
 
 
 def getData(path: str) -> list[list[int]]:
@@ -47,19 +100,38 @@ def getData(path: str) -> list[list[int]]:
     reports = []
     for line in f:
         report = line
-        reports.append(report.split())
+        reports.append(
+            list(
+                map(
+                    int, report.split()
+                )
+            )
+        )
     return reports
 
 
 def main():
     """Main function of day two."""
+    # Part one
     data = getData('data/day-two-test.txt')
     res = partOne(data)
     assert res == 2
 
     data = getData('data/day-two.txt')
     res = partOne(data)
+    assert res == 534
     print(f'Day two part one solution: {res}')
+
+
+    # Part two
+    data = getData('data/day-two-test.txt')
+    res = partTwo(data)
+    assert res == 4
+
+    data = getData('data/day-two.txt')
+    res = partTwo(data)
+    print(f'Day two part two solution: {res}')
+    assert res == 577
     return
 
 
